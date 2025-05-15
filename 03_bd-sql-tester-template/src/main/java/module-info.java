55fc65cc5f03d37907ab62c2e23e587d8f369d18
C14240018 <C14240018@john.petra.ac.id>
2025-05-15 22:38:01 +0700

    Hello message

--- 03_bd-sql-tester-template/src/main/java/module-info.java
+++ 03_bd-sql-tester-template/src/main/java/module-info.java
@@ -5,8 +5,9 @@ module com.example.bdsqltester {
     requires java.sql;
     requires org.slf4j;
 
-
     opens com.example.bdsqltester to javafx.fxml;
+    opens com.example.bdsqltester.dtos to javafx.base; // Baris penting yang ditambahkan
+
     exports com.example.bdsqltester;
     exports com.example.bdsqltester.datasources;
     opens com.example.bdsqltester.datasources to javafx.fxml;
@@ -14,4 +15,7 @@ module com.example.bdsqltester {
     opens com.example.bdsqltester.scenes to javafx.fxml;
     exports com.example.bdsqltester.scenes.admin;
     opens com.example.bdsqltester.scenes.admin to javafx.fxml;
+    exports com.example.bdsqltester.scenes.User;
+    opens com.example.bdsqltester.scenes.User to javafx.fxml;
+    exports com.example.bdsqltester.dtos; // Tambahkan baris ini untuk mengekspor package dtos
 }
